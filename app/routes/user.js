d25042ad1a2b38c0bd579e4bcf40c7e92f4ccfa3
codal-rutvik <rmaheta@codal.com>
2023-11-03 19:48:38 +0530

    update: api routes for the user module
    CIP-651 #time 15m > Updated the api route for the user module api for better api route convention

--- app/routes/user.js
+++ app/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require("../controllers/userController");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.get("/profile", authMiddleware, userController.getUserProfile);
-router.put("/profile", authMiddleware, userController.updateUserProfile);
+router.get("/", authMiddleware, userController.getUserProfile);
+router.put("/", authMiddleware, userController.updateUserProfile);
 
 module.exports = router;
